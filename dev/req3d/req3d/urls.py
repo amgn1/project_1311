e181b8f78367e5fc22126542487fa66ce6063614
Vladimir Volkov <vovanchik4581@gmail.com>
2023-02-23 10:42:30 +0300

    Appform website added

--- dev/req3d/req3d/urls.py
+++ dev/req3d/req3d/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from appform.views import appform_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
+    path('appform/', include('appform.urls'))
 ]
