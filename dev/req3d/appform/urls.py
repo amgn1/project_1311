e4c37b51d474f00137642174b9ce7bd360d105f1
amtrusov <amtrusov@miem.hse.ru>
2023-03-06 13:28:48 +0300

    feat: [Добавили функцию для сохранения формы в БД]

--- dev/req3d/appform/urls.py
+++ dev/req3d/appform/urls.py
@@ -3,6 +3,8 @@ from .views import *
 from django.contrib.auth import views
 
 urlpatterns = [
-    path('', appform_view, name='appform')
+    path('appform/', appform_view, name='appform'),
+    path('add/', add_data, name='add_data'),
+    path('suc/', success, name='success')
    
 ]
--- dev/req3d/appform/views.py
+++ dev/req3d/appform/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from .forms import ArticlesForm
 
 def appform_view(request):
+    print("hello world")
     form = ArticlesForm()
     context = {'form': form}
     return render(request, "appform/appform.html", context)
+def add_data(request):
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            # data = ArticlesForm()
+            # data.Meta.widgets['mail'] = form.cleaned_data.get['mail']
+            form.save()
+            return redirect('add_data')
+    else:
+        form = ArticlesForm()
+    return render(request, 'appform/appform.html', {'form': form})
+    # return render(request, 'appform/appform.html', {'form': form})
+
+def success(request):
+    return render(request, 'success.html')
