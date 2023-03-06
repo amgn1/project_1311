e87b8f7b5cc2b07c48408521daeaf34f8dfb3a2d
amgn1 <dsfunshteyn@miem.hse.ru>
2023-03-06 15:15:59 +0300

    Fix: views.py, urls.py

--- dev/req3d/appform/urls.py
+++ dev/req3d/appform/urls.py
@@ -3,8 +3,5 @@ from .views import *
 from django.contrib.auth import views
 
 urlpatterns = [
-    path('appform/', appform_view, name='appform'),
-    path('add/', add_data, name='add_data'),
-    path('suc/', success, name='success')
-   
+    path('', appform_view, name='appform'),
 ]
--- dev/req3d/appform/views.py
+++ dev/req3d/appform/views.py
@@ -2,24 +2,20 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from .forms import ArticlesForm
+from .models import Articles
+import requests
 
 def appform_view(request):
-    print("hello world")
-    form = ArticlesForm()
-    context = {'form': form}
-    return render(request, "appform/appform.html", context)
-def add_data(request):
     if request.method == 'POST':
-        form = ArticlesForm(request.POST)
-        if form.is_valid():
-            # data = ArticlesForm()
-            # data.Meta.widgets['mail'] = form.cleaned_data.get['mail']
-            form.save()
-            return redirect('add_data')
+        mail = request.POST['mail']
+        name = request.POST['name']
+        if mail and name:
+            data = Articles(mail=mail, name=name)
+            data.save()
+            return render(request,'home/index.html')
     else:
         form = ArticlesForm()
-    return render(request, 'appform/appform.html', {'form': form})
-    # return render(request, 'appform/appform.html', {'form': form})
+        context = {'form': form}
+        return render(request, "appform/appform.html", context)
 
-def success(request):
-    return render(request, 'success.html')
+
