47c4290d6731ee0ac15f4f997c78fe84a83ac36d
amgn1 <106090197+amgn1@users.noreply.github.com>
2023-05-08 20:03:59 +0300

    fix: added id field

--- dev/req3d/bot/models.py
+++ dev/req3d/bot/models.py
@@ -6,3 +6,4 @@ class TgUser(models.Model):
     number = models.CharField(max_length=100)
     user = models.CharField(max_length=100)
     order = models.CharField(default='Нет', max_length=3)
+    user_id = models.CharField(max_length=300)
