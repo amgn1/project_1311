206ffc8465378f90ae81c4b8c3392aba154a6eca
amgn1 <106090197+amgn1@users.noreply.github.com>
2023-05-08 20:04:33 +0300

    fix: fixed 'avatar' issue

--- dev/req3d/oauth/managers.py
+++ dev/req3d/oauth/managers.py
@@ -4,9 +4,13 @@ class KeycloakUserOAuth2Manager(models.UserManager):
 
     def create_new_user(self, user):
         # discord_tag = '%s#%s' % (user['username'], user['discriminator'])
+        try:
+            avatar = user['avatar']
+        except Exception:
+            avatar = None
         new_user = self.create(
             sub=user['sub'],
-            avatar=user['avatar'],
+            avatar=avatar,
             full_name=user['full_name'],
             email=user['email'],
             email_verified=user['email_verified'],
